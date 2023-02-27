0c0d1424b0a78fa1da6c3880e41cb7d9e0e4df5a
Schugggi <95489210+Schugggi@users.noreply.github.com>
2023-02-27 08:41:58 +0100

    update

--- gluecksrad/src/pages/api/getRandomWord.js
+++
@@ -1,9 +0,0 @@
-import { wordList } from "./wordList.js";
-
-export default async function getRandomWord(_, res) {
-//   console.log("Word list: ", wordList)
-  const ranObj = wordList[Math.floor(Math.random() * wordList.length)];
-  const wordObj = { word: ranObj.word };
-  res.statusCode = 200;
-  res.json(wordObj);
-}
